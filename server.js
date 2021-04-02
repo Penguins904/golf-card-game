d92ba9b6ff6c56fbcb8bb407541c659f00d2140c
Jake <jenkinsjake04@gmail.com>
2021-04-01 20:16:03 -0400

    change port to 8080

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const socketio = require('socketio');
 const path = require('path');
 
 const app = express();
-const PORT = 80;
+const PORT = 8080;
 
 //set static folder
 app.use(express.static(path.join(__dirname, "public")));
